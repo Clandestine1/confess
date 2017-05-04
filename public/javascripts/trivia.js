f49ef8d2bec90586ece0d47b1bdddddd0cfd80f0
Clandestine1 <Clandestine.abyss@gmail.com>
2017-05-04 09:51:36 -0400

    four

---
+++ public/javascripts/trivia.js
@@ -0,0 +1,41 @@
+var trivia = [{
+    question: "Whao believes that despair is the stepping stone for hope?",
+    choices: ["Hajime", "Kyoko", "Sakura", "Nagito"],
+    correctAnswer: 3
+}, {
+    question: "Byakuya is the heir to which major company?",
+    choices: ["Togami Industries", "Togami Corporation", " Super Togami", "BT inc."],
+    correctAnswer: 1
+}, {
+    question: "What real game was played during chapter 2 of Danganronpa 2?",
+    choices: ["Super Mario Bros", "Twilight Syndrome Murder Case", "Ultra Despair Girls", "Final Fantasy 7"],
+    correctAnswer: 1
+}, {
+    question: "Who becomes the successor to Junko is Danganronpa Another Episode?",
+    choices: ["Monica", "Nagisa", "Kurokuma", "Kotoko"],
+    correctAnswer: 0
+}, {
+    question: "What is the name of the computer program Chihiro created during the killing game?",
+    choices: ["AI", "Chi Chat", " Alter Ego", "Kuma"],
+    correctAnswer: 2
+}, {
+    question: "Who was inflicted with gullible despair disease during chapter 3 of Danganronpa 2?",
+    choices: ["Nagito", "Hiyoko", "Mikan", "Ibuki"],
+    correctAnswer: 3
+}, {
+    question: "What title was Makoto granted at the end of Danganronpa 1?",
+    choices: ["Ultimate Protagonist", "Ultimate Average Guy", "Ultimate Despair", "Ultimate Hope"],
+    correctAnswer: 3
+}, {
+    question: "Who do the Warriors of Hope loathe?",
+    choices: ["Kids", "Adults", " Hope", "Despair"],
+    correctAnswer: 1
+}, {
+    question: "What real game was played during chapter 2 of Danganronpa 2?",
+    choices: ["Super Mario Bros", "Twilight Syndrome Murder Case", "Ultra Despair Girls", "Final Fantasy 7"],
+    correctAnswer: 2
+}, {
+    question: "Who does Kiyotaka form an unlikely bromance with?",
+    choices: ["Mondo", "Hifumi", "Yasuhiro", "Makoto"],
+    correctAnswer: 0
+}];
